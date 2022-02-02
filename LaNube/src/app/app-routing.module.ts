b0a9c8126c44dccf6038382acc3e525efa3b0f04
fdiemon812 <flavio.dediego@gmail.com>
2022-02-02 17:56:23 +0100

    El milagro, funcionando rutas

--- LaNube/src/app/app-routing.module.ts
+++ LaNube/src/app/app-routing.module.ts
@@ -3,12 +3,20 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login/login.component';
 import { HomeComponent } from './panel-admin/home/home.component';
 import { ValidarLogin } from './guards/validar-login.guard';
+import { ProfesorComponent } from './panel-admin/profesor/profesor.component';
+import { AlumnoComponent } from './panel-admin/alumno/alumno.component';
 
 
 const routes: Routes = [
 
   {path: 'login',component:LoginComponent},
-  {path: 'home',component:HomeComponent, canActivate:[ValidarLogin]}
+  {path: 'home',component:HomeComponent, canActivate:[ValidarLogin], children:[
+
+    {path: 'profesor',component:ProfesorComponent},
+    {path: 'alumno',component:AlumnoComponent}
+
+
+  ]}
   // {path: '**',redirectTo:'login'}
 
 
