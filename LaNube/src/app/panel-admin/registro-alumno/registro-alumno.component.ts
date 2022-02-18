4b5a64f3a6365a62f17b699fa4005ba66ab5e60f
fdiemon812 <flavio.dediego@gmail.com>
2022-02-18 12:40:49 +0100

    formulario

--- LaNube/src/app/panel-admin/registro-alumno/registro-alumno.component.ts
+++ LaNube/src/app/panel-admin/registro-alumno/registro-alumno.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormArray, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { ValidatorAlumnoService } from '../services/validator.alumno.service';
 
 @Component({
@@ -10,18 +10,24 @@ import { ValidatorAlumnoService } from '../services/validator.alumno.service';
 export class RegistroAlumnoComponent implements OnInit {
 
 
-  tutoresExistentes:any[]=[{nombre:"JuanaPaca"},{nombre:"2"},{nombre:"JuanaPaca3"},{nombre:"Arbol"},]
+  // tutoresExistentes:any[]=[{nombre:"JuanaPaca"},{nombre:"2"},{nombre:"JuanaPaca3"},{nombre:"Arbol"},]
 
-  tutoresExistentesLength:any[]=[1]
+  // tutoresExistentesLength:any[]=[1]
 
 
-  tutores:any[]=[];
+  // tutores:any[]=[];
 
     formularioAlumno: FormGroup = this.formBuilder.group({
       nombre: [ , [ Validators.required, Validators.maxLength(100), Validators.pattern(this.validatorAlumService.nombrePattern)]   ],
-      apellidos: [ , [ Validators.required, Validators.maxLength(1),Validators.pattern(this.validatorAlumService.nombrePattern)] ],
+      apellidos: [ , [ Validators.required, Validators.maxLength(100),Validators.pattern(this.validatorAlumService.nombrePattern)] ],
       dni: [ , [ Validators.pattern(this.validatorAlumService.dniPattern)] ],
-      nacimiento: [ , [ Validators.required,this.validatorAlumService.fechaValida]  ]
+      nacimiento: [ , [ Validators.required,this.validatorAlumService.fechaValida]  ],
+      
+      
+      tutores: this.formBuilder.array([])
+
+
+
       // horario: [ '', [ Validators.required, Validators.minLength(3) ] ],
       // favoritos: this.formBuilder.array([], Validators.required )
     })
@@ -32,36 +38,56 @@ export class RegistroAlumnoComponent implements OnInit {
     this.formularioAlumno.reset({})
   }
 
+  // nuevoTutor: FormControl = this.formBuilder.control('', [Validators.required, Validators.minLength(3)] );
+
+  get tutoresArr() {
+    return this.formularioAlumno.get('tutores') as FormArray;
+  }
+
+  agregarTutor() {
+    const tutor = this.formBuilder.group({
+      nombreTutor: new FormControl('', Validators.required),
+      apellidoTutor: new FormControl('',Validators.required),
+      dniTutor: new FormControl('',Validators.required)
+      
+    });
   
+    this.tutoresArr.push(tutor);
+  
+
+  }
+
 
 
   campoNoValido( campo: string ) {
+
+
     return this.formularioAlumno.get(campo)?.invalid
             && this.formularioAlumno.get(campo)?.touched;
   }
 
 
-  addTutor(){
+  // addTutor(){
 
-    const tutor = {
-      id: this.tutores.length+1,
-      nombre:"",
-      apellidos:"",
-      dni:'',
-      tlf:'',
-      email:'',
-      password:''
-    }
+  //   const tutor = {
+  //     id: this.tutores.length+1,
+  //     nombre:"",
+  //     apellidos:"",
+  //     dni:'',
+  //     tlf:'',
+  //     email:'',
+  //     password:''
+  //   }
 
-    this.tutores.push(tutor);
-  }
+  //   this.tutores.push(tutor);
+  // }
 
-  addTutorExistente(){
+  // addTutorExistente(){
 
     
 
-    this.tutoresExistentesLength.push(1);
-  }
+  //   this.tutoresExistentesLength.push(1);
+  // }
 
 
   get nombreErrorMsg(): string {
@@ -122,6 +148,7 @@ export class RegistroAlumnoComponent implements OnInit {
     console.log("hola")
     console.log(this.formularioAlumno.valid)
     console.log(this.formularioAlumno.value)
+    this.findInvalidControlsRecursive(this.formularioAlumno)
     if(this.formularioAlumno.valid){
 
       console.log("enviando!!!")
@@ -129,6 +156,25 @@ export class RegistroAlumnoComponent implements OnInit {
   }
 
 
+  // comprueba que campos no están validando en el formulario
+  public findInvalidControlsRecursive(formToInvestigate:FormGroup|FormArray):string[] {
+    var invalidControls:string[] = [];
+    let recursiveFunc = (form:FormGroup|FormArray) => {
+      Object.keys(form.controls).forEach(field => { 
+        const control = form.get(field);
+        if (control?.invalid) invalidControls.push(field);
+        if (control instanceof FormGroup) {
+          recursiveFunc(control);
+        } else if (control instanceof FormArray) {
+          recursiveFunc(control);
+        }        
+      });
+    }
+    recursiveFunc(formToInvestigate);
+    console.log(invalidControls)
+    console.log(this.formularioAlumno.valid + "gfgdfgdgdfgfdgfdgdfg")
+    return invalidControls;
+  }
 
 
 }
