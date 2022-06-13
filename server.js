f28b8dcf496000b458bfcd52e2462c1f6b81bf08
A860871 <flavio.de.external@atos.net>
2022-06-13 22:24:04 +0200

    arreglando server

--- server.js
+++ server.js
@@ -1,5 +1,12 @@
 const express = require('express');
 const path = require('path');
-const app = express();app.use(express.static(__dirname + '/dist/la-nube'));
-app.get('*', function(req,res) {res.sendFile(path.join(__dirname+'/dist/la-nube/index.html'));});
-app.listen(process.env.PORT || 8080);
+const app = express();
+app.use(express.static(path.join(__dirname, 'dist','la-nube')));
+app.get('*', function(req,res) {
+
+  res.sendFile(path.join(__dirname,'dist','app_name','index.html'));
+
+})
+
+  app.listen(process.env.PORT || 8080);
+
